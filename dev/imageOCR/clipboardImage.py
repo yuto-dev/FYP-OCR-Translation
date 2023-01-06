8be855dc0778d684dd87524402d868f2b2e5be52
yuto-dev <directx178@gmail.com>
2023-01-06 12:17:59 +0800

    Image OCR finished
    Added OCR capabilities on images where user can either import image files directly or copy the image to clipboard.

--- dev/imageOCR/clipboardImage.py
+++ dev/imageOCR/clipboardImage.py
@@ -1,26 +1,11 @@
-# importing required modules
-import PyPDF2
-
-# creating a pdf file object
-pdfFileObj = open('jacatra.pdf', 'rb')
-
-# creating a pdf reader object
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-
-# printing number of pages in pdf file
-print(pdfReader.numPages)
-
-# creating a page object
-pageObj = pdfReader.getPage(6)
-
-# extracting text from page
-print(pageObj.extractText())
-
-# closing the pdf file object
-pdfFileObj.close()
-
+from PIL import ImageGrab, Image
 import cv2
 import pytesseract
+
+img = ImageGrab.grabclipboard()
+
+img.save('pil.jpg')
+
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
@@ -28,7 +13,8 @@ def ocr_core(img):
     text = pytesseract.image_to_string(img)
     return text
 
-img = cv2.imread('image.jpg')
+
+img = cv2.imread('pil.jpg')
 
 def get_grayscale(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
